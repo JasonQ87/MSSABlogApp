6d4ce935b954710807aebf5a2381df068ea23e3c
jason <jason.m.qualls@outlook.com>
2026-01-08 10:12:33 +0300

    Add project files.

---
+++ MSSABlogApp/Model/BloggingContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSSABlogApp
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        //Configuring Connecgtionstring that will be used by BloggingContext to connect
+        //the backend database.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BloggingDB;Integrated Security=True;Encrypt=False");
+        }
+    }
+}
---
+++ MSSABlogApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MSSABlogApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
